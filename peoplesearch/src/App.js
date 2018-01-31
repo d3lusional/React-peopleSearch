36d9e7db356363bb4b36e1e5eac5b9adacee019a
Del <del.larsen@gmail.com>
2018-01-31 16:09:24 -0600

    updates

--- peoplesearch/src/App.js
+++ peoplesearch/src/App.js
@@ -1,51 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+
 import './App.css';
+import { 
+  SEARCH,
+  USER_SELECTED 
 
-const genId = (str1, str2, str3) => {
-  const megaStr = '' + str1 + str2 + str3;
-  const chars = [];
-  for(let i = 0; i < megaStr.length; i++) {
-    const randomVal = Math.floor(Math.random() * 3 * megaStr.charCodeAt(i));
-    if (randomVal % 3 === 0) {
-      chars.push(i);
-    } else {
-      chars.push(String.fromCharCode(randomVal));
-    } if(i === str1.length || i === str2.length) chars.push('-')
+} from './index';
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = (state) => {
+  return {
+      userPage: state.userPage,
+      users: state.users,
+      search: state.search,
+    }
   }
-  return chars.join('');
-}
-
-class User {
-  constructor(
-    name,
-    city,
-    industry,
-    hobbies,
-    email
-  ) {
-    this.name = name;
-    this.city = city;
-    this.industry = industry;
-    this.hobbies = hobbies;
-    this.email = email;
-    this.id = genId(email, industry, city);
+  
+  const mapDispatchToProps = (dispatch) => {
+  return {
+      userSelected: function(user){
+        dispatch({
+          type: USER_SELECTED,
+          payload: user     
+          }
+        )
+      },
+      searchUsers: function(newSearch) { 
+        dispatch(
+          {
+            type: SEARCH,
+            payload: newSearch
+          }
+        )
+      }
+    }
   }
-}
 
-const initialState = {
-  userPage: undefined,
-  users: [
-    new User('Bobby', 'Los Angeles', 'Software Development', 'Many many awesome fun hobbies', 'email@email.com'),
-    new User('Henry', 'Seattle', 'Software Production', 'TV shows', 'root@email.com'),
-    new User('Sofie', 'Boulder', 'Software Engineer', 'Gardening', 'souped up@email.com'),
-    new User('Miranda', 'Detroit', 'Mechanic', 'Video Games', 'trippers@email.com'),
-    new User('Jerome', 'NYC', 'Physicist', 'Reading', 'email@mailamail.com'),
-    new User('Millie', 'Hawkins, Indiana', 'ESP', 'Blowing up things from the upside down', 'hoppin@email.com'),
-    new User('Train', 'Oaklahoma City', 'Real Engineer', 'choo choo', 'chooc.choo@email.com'),
-  ],
-  search: ''
-}
+
 
 
 
@@ -54,42 +47,48 @@ class App extends Component {
   constructor(props){
     super(props);
     
-    this.state = initialState
+   
 
     
   }
 
-
-  updateSearch(event){
-    this.setState({search: event.target.value})
-  }
   render() {
 
-    let filteredUsers = this.state.users.filter(
-        (user) => {
-          return user.name.indexOf(this.state.search) !== -1;
-        }
-    )
+    const filteredUsers = this.props.search === '' ? this.props.users : this.props.users.filter ((user) => user.name.includes(this.props.search))
+
     return (
+      this.props.userPage === undefined ? 
 
       <div className="App">
-        <header>
-        </header>
         <div className="row">
-            <input className="" value={this.state.search} onChange={this.updateSearch.bind(this)} />
+            <input className="" value={this.props.search} onChange={ event => this.props.searchUsers(event.target.value)} />
         </div>
         <div className="row">
-            <div >
-           
-            {filteredUsers.map((user)=> {
-              return <p key="id"> {user.name}</p>
+            <div>
+            {filteredUsers.map((user, index)=> {
+              return <p key={index} onClick={ () => this.props.userSelected(user) } > {user.name} </p>
             })}
             </div>
         </div>
-       
+      </div>
+
+      :
+      
+      <div className="App"> 
+        {console.log(this.props.userPage)}
+          <div className="usersBox" >
+            
+            <p onClick={ () => this.props.userSelected() } > {}  Go Home</p>
+             <p>{this.props.userPage.name}</p>
+             <p>{this.props.userPage.city}</p>
+             <p>{this.props.userPage.industry}</p>
+             <p>{this.props.userPage.hobbies}</p>
+             <p>{this.props.userPage.email}</p>
+          </div>
+      
       </div>
     );
   }
 }
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
-export default App;
--- peoplesearch/src/index.js
+++ peoplesearch/src/index.js
@@ -3,6 +3,84 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PropTypes from 'prop-types';
+export const SEARCH = 'thdfghf';
+export const USER_SELECTED = 'asldjkfhalkdfjh';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const genId = (str1, str2, str3) => {
+    const megaStr = '' + str1 + str2 + str3;
+    const chars = [];
+    for(let i = 0; i < megaStr.length; i++) {
+      const randomVal = Math.floor(Math.random() * 3 * megaStr.charCodeAt(i));
+      if (randomVal % 3 === 0) {
+        chars.push(i);
+      } else {
+        chars.push(String.fromCharCode(randomVal));
+      } if(i === str1.length || i === str2.length) chars.push('-')
+    }
+    return chars.join('');
+  }
+
+
+
+class User {
+    constructor(
+      name,
+      city,
+      industry,
+      hobbies,
+      email
+    ) {
+      this.name = name;
+      this.city = city;
+      this.industry = industry;
+      this.hobbies = hobbies;
+      this.email = email;
+      this.id = genId(email, industry, city);
+    }
+  }
+  
+  const initialState = {
+    userPage: undefined,
+    users: [
+      new User('Bobby', 'Los Angeles', 'Software Development', 'Many many awesome fun hobbies', 'email@email.com'),
+      new User('Henry', 'Seattle', 'Software Production', 'TV shows', 'root@email.com'),
+      new User('Sofie', 'Boulder', 'Software Engineer', 'Gardening', 'souped up@email.com'),
+      new User('Miranda', 'Detroit', 'Mechanic', 'Video Games', 'trippers@email.com'),
+      new User('Jerome', 'NYC', 'Physicist', 'Reading', 'email@mailamail.com'),
+      new User('Millie', 'Hawkins, Indiana', 'ESP', 'Blowing up things from the upside down', 'hoppin@email.com'),
+      new User('Train', 'Oaklahoma City', 'Real Engineer', 'choo choo', 'chooc.choo@email.com'),
+    ],
+    search: ''
+  }
+
+function reducer(state=initialState, action) {
+    switch (action.type) {
+        case SEARCH:
+            console.log("hello from search redux")
+            return {...state, search: action.payload} 
+        case USER_SELECTED:
+            return {...state, userPage: action.payload}
+        default:
+        return state;
+    }
+}
+
+
+const store = createStore(
+    reducer
+    )
+
+const Root = () => (
+
+    <Provider store={store} >
+        <App />
+    </Provider>
+)
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
